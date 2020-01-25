3601c21cac96f6704ad106525cb73530796c982d
Philip Monk <phil@pcmonk.me>
2020-01-24 16:20:13 -0800

    king: style improvements

--- pkg/hs/urbit-king/lib/Urbit/King/Main.hs
+++ pkg/hs/urbit-king/lib/Urbit/King/Main.hs
@@ -272,7 +272,7 @@ collectAllFx top = do
         logTrace "Done collecting effects!"
   where
     tmpDir :: FilePath
-    tmpDir = top <> "/.tmpdir"
+    tmpDir = top </> ".tmpdir"
 
     collectedFX :: RAcquire e ()
     collectedFX = do
